5b73c65cf2ea85d40dc01e3c1d18b533f8ae27fc
christinaadanks <christinaa.danks@me.com>
2022-04-29 22:22:10 -0700

    updated to better algorithm

--- easy-tests/test_two_sum.py
+++ easy-tests/test_two_sum.py
@@ -9,8 +9,8 @@ class TestTwoSum(TestCase):
         nums2 = [3, 2, 4]
         target2 = 6
         nums3 = [3, 3]
-        self.assertEqual([0, 1], two_sum(nums, target))
-        self.assertEqual([1, 2], two_sum(nums2, target2))
-        self.assertEqual([0, 1], two_sum(nums3, target2))
+        self.assertEqual([1, 0], two_sum(nums, target))
+        self.assertEqual([2, 1], two_sum(nums2, target2))
+        self.assertEqual([1, 0], two_sum(nums3, target2))
 
 
--- easy/two_sum.py
+++ easy/two_sum.py
@@ -1,10 +1,8 @@
 def two_sum(nums, target):
     hashmap = {}
     for i in range(len(nums)):
-        # key = number, value = index of number
-        hashmap[nums[i]] = i
-    for i in range(len(nums)):
         # find difference and see if it is in hashmap
         diff = target - nums[i]
-        if diff in hashmap and hashmap[diff] != i:
+        if diff in hashmap:
             return [i, hashmap[diff]]
+        hashmap[nums[i]] = i
